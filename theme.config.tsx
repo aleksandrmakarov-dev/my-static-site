8e2a7106fe9ea33d6784e9cbeda37c75cceccfb2
alex <ac5295@student.jamk.fi>
2024-08-07 23:07:10 +0300

    jhghjg

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>NETCODE.SPACE</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/aleksandrmakarov-dev/my-static-site",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
 
-export default config
+  chat: {
+    link: "https://discord.com",
+  },
+  docsRepositoryBase: "https://github.com/aleksandrmakarov-dev/my-static-site",
+  footer: {
+    text: "NETCODE.SPACE 2024",
+  },
+};
+
+export default config;
